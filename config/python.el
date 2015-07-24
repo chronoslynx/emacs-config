c0166e1b42e17f69e57c1fbe0085f6967dea78df
Tim Swihart <Timothy.Swihart@gtri.gatech.edu>
2015-07-24 14:30:19 -0400

    initial commit

---
+++ config/python.el
@@ -0,0 +1,6 @@
+(add-hook 'python-mode-hook 'jedi:setup)
+(defun my/python-mode-hook ()
+  (add-to-list 'company-backends 'company-jedi))
+
+(add-hook 'python-mode-hook 'my/python-mode-hook)
+(provide 'python)
---
+++ init.el
@@ -0,0 +1,79 @@
+;; (Shamelessly based on chrisdone's emacs config)
+;; Standard libraries needed
+(add-to-list 'load-path "~/.emacs.d/packages/dash")
+(require 'dash)
+
+;; Packages to laod
+(defvar packages
+  '(better-defaults
+    company
+    company-jedi
+    flycheck
+    flycheck-pylama
+    god-mode
+    helm
+    helm-projectile
+    markdown-mode
+    projectile
+    rainbow-delimeters
+    ;rust-mode
+    sml-mode
+    )
+  "Packages whose location follows the
+  packages/package-name/package-name.el format.")
+
+(defvar configs
+  '("global"
+    "god"
+    "golang"
+    "haskell"
+    "prelude-helm-everywhere"
+    "markdown"
+    "my-magit"
+    "rust"
+    "python")
+  "Configuration files that follow the config/foo.el path
+  format.")
+
+(defvar custom-load-paths
+  '("company-mode"
+    "emacs-async"
+    "flx"
+    "jedi"
+    "helm"
+    "magit/lisp"))
+
+;; Add custom load paths
+(let (s)
+  (-each custom-load-paths
+    (lambda (location)
+        (add-to-list 'load-path
+	       (concat (file-name-directory (or load-file-name
+						(buffer-file-name)))
+		       "packages/"
+		       location)))))
+;; Load packages
+(let (s)
+  (-each packages
+    (lambda (name)
+           (progn (unless (fboundp name)
+                    (add-to-list 'load-path
+                                 (concat (file-name-directory (or load-file-name
+                                                                  (buffer-file-name)))
+                                         "packages/"
+                                         (symbol-name name)))
+                    (require name))))))
+
+;; Load configurations
+(let (s)
+  (-each configs
+    (lambda (name)
+      (load (concat (file-name-directory load-file-name)
+                    "config/"
+                    name ".el")))))
+
+;; Mode initializations
+(require 'helm-config)
+(projectile-global-mode)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(god-mode)
