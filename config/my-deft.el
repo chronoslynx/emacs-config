1ef845699e18d12ffd922d09006e87f2fcdadf0d
Tim Swihart <Timothy.Swihart@gtri.gatech.edu>
2015-07-27 15:24:33 -0400

    trailing slash

--- config/my-deft.el
+++ config/my-deft.el
@@ -1,6 +1,6 @@
 (require 'deft)
 (setq deft-extensions '("markdown" "md" "txt" "tex" "org"))
-(setq deft-directory "~/Dropbox/Notational Velocity")
+(setq deft-directory "~/Dropbox/Notational Velocity/")
 (setq deft-auto-save-interval 0.0)
 (global-set-key [f8] 'deft)
 (global-set-key (kbd "C-x C-g") 'deft-find-file)
