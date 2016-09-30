f30ee382f2d7a78dca8761cef325a9d317b38e41
Tim Swihart <chronoslynx@users.noreply.github.com>
2016-09-30 09:59:11 -0400

    set ripgrep as default search program

--- init.el
+++ init.el
@@ -4,15 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode nil)
+ '(counsel-ag-base-command "~/.cargo/bin/rg --color=never %s")
  '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
  '(persp-show-modestring t)
- '(safe-local-variable-values
-   (quote
-    ((pyenv-mode-set "apiary")
-     (pyenv-mode-set "warthog"))))
  '(z3-solver-cmd "/usr/local/bin/z3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
